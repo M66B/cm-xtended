1cd346a29bd212b7fa69682246bc2431420082ee
Marcel Bokhorst <marcel@bokhorst.biz>
2013-05-06 19:39:06 +0200

    Fix repo download

--- merge_requests.sh
+++ merge_requests.sh
@@ -7,7 +7,7 @@ fi
 cd ${android}
 repo download frameworks/av/ 32906
 repo download packages/apps/LegacyCamera 28336
-repo download hardware/qcom/audio-caf 34989
+#repo download hardware/qcom/audio-caf 34989
 #repo download hardware/ti/wpan 36772
 repo download hardware/qcom/audio-caf 37046
 
@@ -19,9 +19,9 @@ repo download hardware/qcom/audio-caf 37046
 #cd ${android}/packages/apps/LegacyCamera/
 #git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_LegacyCamera refs/changes/36/28336/1 && git format-patch -1 --stdout FETCH_HEAD | patch -p1
 
-#http://review.cyanogenmod.org/#/c/34989/
-#cd ${android}/hardware/qcom/audio-caf
-#git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/89/34989/7 && git format-patch -1 --stdout FETCH_HEAD | patch -p1
+http://review.cyanogenmod.org/#/c/34989/
+cd ${android}/hardware/qcom/audio-caf
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/89/34989/7 && git format-patch -1 --stdout FETCH_HEAD | patch -p1
 
 #http://review.cyanogenmod.org/#/c/36772/
 #cd ${android}/hardware/ti/wpan
