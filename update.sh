c5e99dd9759ba5392464846b9b1b71aa07dd0ccb
Marcel Bokhorst <marcel@bokhorst.biz>
2013-05-11 16:45:45 +0200

    BlueDroid / TI driver compatibility

--- update.sh
+++ update.sh
@@ -429,6 +429,8 @@ fi
 #Legacy camera
 #do_replace "#BOARD_USES_LEGACY_CAMERA := true" "BOARD_USES_LEGACY_CAMERA := true" ${android}/device/semc/msm7x30-common/BoardConfigCommon.mk
 
+do_replace "#define BTHC_USERIAL_READ_MEM_SIZE (1024)" "#define BTHC_USERIAL_READ_MEM_SIZE (2048)" ${android}/external/bluetooth/bluedroid/hci/include/bt_hci_bdroid.h
+
 #Boot logo
 if [ "${bootlogo}" = "Y" ]; then
 	echo "*** Boot logo ***"
