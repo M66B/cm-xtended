078b8513341991049ee99441f6bd45b29222aa77
Marcel Bokhorst <marcel@bokhorst.biz>
2013-05-22 19:02:41 +0200

    DASH 0 lux merged

--- merge_requests.sh
+++ merge_requests.sh
@@ -21,11 +21,6 @@ repo download packages/apps/LegacyCamera 28336/1
 #Add FM audio routing
 repo download hardware/qcom/audio-caf 34989/7
 
-#http://review.cyanogenmod.org/#/c/38104/
-#Revert "Allow brightness decrease when sensor value is zero"
-#allows us to use again the lowest brightness with 0 lux
-repo download hardware/sony/DASH 38104/1
-
 #http://review.cyanogenmod.org/#/c/38174/
 #init: Fix serial number on semc bootloaders
 repo download system/core 38174/1
