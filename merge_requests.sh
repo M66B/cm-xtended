cd14cc4c9e5d2628640ef57285c5ea1692dfcb41
Michael Bestas <mikeioannina@gmail.com>
2013-07-24 23:24:28 +0300

    Update merge_requests.sh

--- merge_requests.sh
+++ merge_requests.sh
@@ -4,17 +4,20 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-cd ${android}
 
-#http://review.cyanogenmod.org/#/c/34989/
-#msm7x30: Add support for SEMC FM radio
-#Add FM audio routing
-#repo download hardware/qcom/audio-caf 34989/7
+cd ${android}/system/core
+# init: Fix serial number on semc bootloaders
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/74/38174/1 && git cherry-pick FETCH_HEAD
 
-#http://review.cyanogenmod.org/#/c/38174/
-#init: Fix serial number on semc bootloaders
-repo download system/core 38174/1
 
-#http://review.cyanogenmod.org/#/c/45435/
-#Add power off mode in advanced menu
-repo download bootable/recovery 45435/1
+cd ${android}/bootable/recovery
+# "not enough rainbows, 1 star uninstall"
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/77/36777/3 && git cherry-pick FETCH_HEAD
+# Update wipe data option confirmation
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/37/45437/6 && git cherry-pick FETCH_HEAD
+# Add sdparted option to partition in ext4 fstype
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/57/45557/2 && git cherry-pick FETCH_HEAD
+# Enable partition sdcard when non vfat fstype2 option is used in recovery.fstab
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/58/45558/2 && git cherry-pick FETCH_HEAD
+# Fix partition sdcard menu options
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/61/45561/7 && git cherry-pick FETCH_HEAD
