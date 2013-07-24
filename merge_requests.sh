eb22a19653717d6db4566e29481c90fbee3b1bf1
Michael Bestas <mikeioannina@gmail.com>
2013-07-24 23:45:02 +0300

    Remove broken patches

--- merge_requests.sh
+++ merge_requests.sh
@@ -17,7 +17,3 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery re
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/37/45437/6 && git cherry-pick FETCH_HEAD
 # Add sdparted option to partition in ext4 fstype
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/57/45557/2 && git cherry-pick FETCH_HEAD
-# Enable partition sdcard when non vfat fstype2 option is used in recovery.fstab
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/58/45558/2 && git cherry-pick FETCH_HEAD
-# Fix partition sdcard menu options
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/61/45561/7 && git cherry-pick FETCH_HEAD
